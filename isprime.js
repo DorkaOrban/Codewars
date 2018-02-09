00e2651427484da6560d0bf3a6f1d98657280825
DorkaOrban <dorkaorban369@yahoo.com>
2018-02-09 22:18:13 +0200

    Create isprime.js

---
+++ isprime.js
@@ -0,0 +1,20 @@
+/*
+Is a number prime?
+Level: 6 kyu
+
+Problem description:
+Define a function isPrime/is_prime() that takes one integer argument and returns true/True or false/False depending on if the integer is a 
+prime.
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Example
+isPrime(5)
+=> true
+*/
+
+const isPrime = num => {
+    if(num < 2) return false;
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num !== 1;
+}
