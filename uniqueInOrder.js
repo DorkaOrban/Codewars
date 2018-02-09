040c3792744bc1aeb1891b8de19c437c0278fc3f
DorkaOrban <dorkaorban369@yahoo.com>
2018-02-09 22:30:02 +0200

    Update uniqueInOrder.js

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -4,7 +4,8 @@ Unique In Order
 Level: 6 kyu
 
 Problem description:
-Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the
+same value next to each other and preserving the original order of elements.
 
 For example:
 uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
