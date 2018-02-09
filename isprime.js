947d22478b36a6bb07dee5d47dc9123bb1d9074f
DorkaOrban <dorkaorban369@yahoo.com>
2018-02-09 22:18:34 +0200

    Update isprime.js

--- isprime.js
+++ isprime.js
@@ -3,8 +3,8 @@ Is a number prime?
 Level: 6 kyu
 
 Problem description:
-Define a function isPrime/is_prime() that takes one integer argument and returns true/True or false/False depending on if the integer is a 
-prime.
+Define a function isPrime/is_prime() that takes one integer argument and returns true/True or false/False depending on if the integer 
+is a prime.
 Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
 Example
